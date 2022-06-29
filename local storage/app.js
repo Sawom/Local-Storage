e246ebe001943323af7dc3618ea90a23ea89a517
Abdur Rashid Sawom <64780532+Sawom@users.noreply.github.com>
2022-06-29 11:46:12 +0600

    Add files via upload

---
+++ local storage/app.js
@@ -0,0 +1,36 @@
+// add data
+const addData = (item)=>{
+    let data = getData();
+    if(item in data){
+        data[item] = data[item]+1;
+    }
+    else{
+        data[item] = 1;
+    }
+    console.log(data);
+    saveData(data);
+}
+// remove data
+const removeData = (item)=>{
+    let data = getData();
+    delete data[item];
+    saveData(data);
+}
+// save data
+const saveData = (data)=>{
+    console.log(data);
+    let savedb = JSON.stringify(data);
+    console.log(savedb);
+    localStorage.setItem('cart', savedb);
+}
+//get data
+const getData = ()=>{
+    let saved = localStorage.getItem('cart');
+    if(saved){
+        saved = JSON.parse(saved);
+    }else{
+        saved = {};
+    }
+    
+    return saved;
+}
